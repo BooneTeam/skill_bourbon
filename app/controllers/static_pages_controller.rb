dacd5900c219d4d870b6fb5aa98fdafdf97f8a65
Garrett Boone <garrettboone@garretts-mbp-2.attlocal.net>
2014-08-12 22:19:14 -0500

    intial

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,27 @@
+class StaticPagesController < ApplicationController
+
+  def home
+
+  end
+
+  def learning
+
+  end
+
+  def earning
+
+  end
+
+  def what_is
+
+  end
+
+  def faq
+
+  end
+
+  def contact
+
+  end
+
+end
