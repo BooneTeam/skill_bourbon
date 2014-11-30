fdae50d2e5d5716df828380bf5fc4d23bcc89bc6
Garrett Boone <garrettboone@garretts-mbp-2.attlocal.net>
2014-11-30 14:59:10 -0600

    yaml

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,6 +4,7 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
+  binding.pry
   config.secret_key = Figaro.env.secret_key_base
 
   # ==> Mailer Configuration
