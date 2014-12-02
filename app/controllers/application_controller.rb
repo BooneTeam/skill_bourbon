ad813fa5fd7b2761df8f09fcb63f835fffd4bd94
Garrett Boone <garrettboone@garretts-mbp-2.attlocal.net>
2014-12-01 22:17:08 -0600

    add beta message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  def is_beta?
+    true
+  end
+
   # add_flash_types :error
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   # redirect back to page where user came from after logging in.
   def after_sign_in_path_for(resource)
     sign_in_url = url_for(:action => 'new', :controller => 'devise/sessions', :only_path => false, :protocol => 'http')
@@ -16,8 +20,12 @@ class ApplicationController < ActionController::Base
   end
 
   def login_required
-    if current_user
+    if (is_beta? && current_user.username.include?("test_beta"))
       session[:return_to_url] = request.url
+    elsif (is_beta? && !current_user.username.include?("test_beta"))
+      redirect_to what_is_path
+    elsif current_user && !is_beta?
+        session[:return_to_url] = request.url
     else
       redirect_to new_user_registration_path
     end
